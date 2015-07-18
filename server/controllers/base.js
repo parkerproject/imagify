6ad80b7103803d231a6b6192f3ecbd51e5c571a2
Parker <parkerproject@gmail.com>
2015-07-18 08:00:15 -0400

    updates

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -4,15 +4,13 @@ var swig = require('swig');
 var fs = require('fs');
 var _request = require('request');
 var Promise = require('es6-promise').Promise;
+var imagify = require('./image');
+var Joi = require('joi');
+var uid = require('uid-safe');
 var lwip = require('lwip');
-var download = function (uri, filename, callback) {
-  _request.head(uri, function (err, res, body) {
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    _request(uri).pipe(fs.createWriteStream('img/' + filename)).on('close', callback);
-  });
-};
+var sha1 = require('sha1');
+var EventEmitter = require('events').EventEmitter;
+var fileCreated = new EventEmitter();
 
 
 
@@ -20,37 +18,56 @@ module.exports = {
   index: {
     handler: function (request, reply) {
 
+
       var url = request.query.url;
-      var name = 'test.jpeg';
+      var name = sha1(url) + '.jpeg';
       var img_path = 'img/' + name;
-      var resize_width = request.query.resize[0];
-      var resize_height = request.query.resize[1];
-      //var crop_width = request.query.crop[0];
-      //var crop_height = request.query.crop[1];
 
 
-      download(url, name, function () {
+      fileCreated.on(img_path, function (ray) {
+        console.log(ray);
+        setTimeout(function () {
+          fs.unlink(img_path, function (err, results) {
+            if (err) console.log('File Doesnt exists');
+            else console.log('deleted!');
+          });
+        }, 10000);
 
-      lwip.open(img_path, function(err, image){
+      });
 
-        image.batch()
-          //.scale(0.75) // scale to 75%
-          //.rotate(45, 'white') // rotate 45degs clockwise (white fill)
-          //.crop(width, height) // crop a 200X200 square from center
-          .resize(resize_width, resize_height)
-          //.blur(5) // Gaussian blur with SD=5
-          .writeFile(img_path, function (err) {
-            // check err...
-            reply.file(img_path);
+
+      return new Promise(function (resolve) {
+        imagify.download(url, name, function () {
+
+          lwip.open(img_path, function (err, image) {
+
+            var width = parseInt(request.query.resize_width) || image.width();
+            var height = parseInt(request.query.resize_height) || image.height();
+
+            image.resize(width, height, function (err, img) {
+              img.writeFile(img_path, function (err) {
+                resolve();
+              });
+            });
+
           });
 
+        });
+      }).then(function () {
+        return new Promise(function (res) {
+          fileCreated.emit(img_path, 'GAMMA');
+          reply.file(img_path);
+        });
       });
-
-      });
-
     },
-    app: {
-      name: 'index'
+    validate: {
+      query: {
+        url: Joi.string(),
+        resize_width: Joi.string(),
+        resize_height: Joi.string(),
+        crop_width: Joi.string(),
+        crop_height: Joi.string()
+      }
     }
   }
 };
--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,1 +1,28 @@
+var lwip = require('lwip');
+var fs = require('fs');
+var _request = require('request');
 
+
+
+function download(uri, filename, callback) {
+  'use strict';
+  _request.head(uri, function (err, res, body) {
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    _request(uri).pipe(fs.createWriteStream('img/' + filename)).on('close', callback);
+  });
+}
+
+function crop_image(img_path, width, height) {
+  'use strict';
+  lwip.open(img_path, function (err, image) {
+    image.batch()
+      .crop(width, height)
+      .writeFile(img_path, function (err) {
+        console.log('image cropped');
+      });
+  });
+}
+
+module.exports.crop_image = crop_image;
+module.exports.download = download;
