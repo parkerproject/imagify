65e98fb7dcefc13496d70b540ab3cbbbf54dcdc7
parkerproject <parkerproject@gmail.com>
2015-07-22 15:40:18 +0000

    updates

--- server/controllers/sendEmail.js
+++ server/controllers/sendEmail.js
@@ -1,8 +1,7 @@
 require('dotenv').load();
-var domain = 'sandbox069a93c4f3434558b73eb6e0e0cf7a42.mailgun.org';
 var mailgun = require('mailgun-js')({
-  apiKey: process.env.DEV_MAILGUN_KEY,
-  domain: process.env.DEV_MAILGUN_DOMAIN
+  apiKey: process.env.MAILGUN_KEY,
+  domain: process.env.MAILGUN_DOMAIN
 });
 
 
