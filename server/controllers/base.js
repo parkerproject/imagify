7c5052a125fa9af8e11d0202cdba8ebf691ef573
Parker <parkerproject@gmail.com>
2015-07-23 16:55:11 -0400

    updates

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -1,96 +1,123 @@
 // This is the base controller. Used for base routes, such as the default index/root path, 404 error pages, and others.
 require('dotenv').load();
-var swig = require('swig');
 var fs = require('fs');
-var _request = require('request');
 var Promise = require('es6-promise').Promise;
 var imagify = require('./image');
 var Joi = require('joi');
-var uid = require('uid-safe');
 var lwip = require('lwip');
 var sha1 = require('sha1');
 var EventEmitter = require('events').EventEmitter;
 var fileCreated = new EventEmitter();
-var url = require('url');
+var http = require('http');
+var imageType = require('image-type');
 
 function ratio(width, height) {
   return width / height;
 }
 
+var acceptedExt = ['jpeg', 'png', 'gif', 'jpg', 'JPG', 'JPEG', 'PNG', 'GIF'];
+
 
 module.exports = {
   img: {
     handler: function (request, reply) {
-
+      "use strict";
 
       var url = request.query.url;
-      var name = sha1(url) + '.jpeg';
-      var img_path = 'img/' + name;
+      var fileType, typeObj;
 
 
-      fileCreated.on(img_path, function (ray) {
-        console.log(ray);
-        setTimeout(function () {
-          fs.unlink(img_path, function (err, results) {
-            if (err) console.log('File Doesnt exists');
-            else console.log('deleted!');
-          });
-        }, 10000);
+      http.get(url, function (res) {
+        res.once('data', function (chunk) {
+          res.destroy();
+          typeObj = imageType(chunk);
+          fileType = (typeObj !== null) ? typeObj.ext : null;
 
-      });
+          if (acceptedExt.indexOf(fileType === -1) || fileType !== null) {
 
+            var name = sha1(url) + '.' + fileType;
+            var img_path = 'img/' + name;
 
-      return new Promise(function (resolve) {
-        imagify.download(url, name, function () {
+            fileCreated.on(img_path, function (ray) {
+              console.log(ray);
+              setTimeout(function () {
+                fs.unlink(img_path, function (err, results) {
+                  if (err) console.log('File Doesnt exists');
+                  else console.log('deleted!');
+                });
+              }, 10000);
 
-          lwip.open(img_path, function (err, image) {
-            var width, height, ifNotGiven, batch = image.batch();
-
-            if (request.query.resize_width || request.query.resize_height) {
-
-              if (request.query.resize_width && request.query.resize_height) {
-
-                width = parseInt(request.query.resize_width);
-                height = parseInt(request.query.resize_height);
-
-              } else if (request.query.resize_width && !request.query.resize_height) {
-
-                width = parseInt(request.query.resize_width);
-                height = width / ratio(image.width(), image.height());
-
-              } else if (!request.query.resize_width && request.query.resize_height) {
-
-                height = parseInt(request.query.resize_height);
-                width = height * ratio(image.width(), image.height());
-              } else {
-
-                width = image.width();
-                height = image.height();
-              }
-
-              batch.resize(width, height);
-
-            }
-
-            if (request.query.crop_width && request.query.crop_height) {
-
-              batch.crop(parseInt(request.query.crop_width), parseInt(request.query.crop_height));
-
-            }
-
-            batch.writeFile(img_path, function (err) {
-              resolve();
             });
 
-          });
+
+            return new Promise(function (resolve) {
+              imagify.download(url, name, function () {
+
+                lwip.open(img_path, function (err, image) {
+
+                  if (image === null) {
+                    fileCreated.emit(img_path, 'GAMMA');
+                    reply('image is invalid');
+                  }
+
+                  if (image !== null) {
+
+                    var width, height, ifNotGiven, batch = image.batch();
+
+                    if (request.query.resize_width || request.query.resize_height) {
+
+                      if (request.query.resize_width && request.query.resize_height) {
+
+                        width = parseInt(request.query.resize_width);
+                        height = parseInt(request.query.resize_height);
+
+                      } else if (request.query.resize_width && !request.query.resize_height) {
+
+                        width = parseInt(request.query.resize_width);
+                        height = width / ratio(image.width(), image.height());
+
+                      } else if (!request.query.resize_width && request.query.resize_height) {
+
+                        height = parseInt(request.query.resize_height);
+                        width = height * ratio(image.width(), image.height());
+                      } else {
+
+                        width = image.width();
+                        height = image.height();
+                      }
+
+                      batch.resize(width, height);
+
+                    }
+
+                    if (request.query.crop_width && request.query.crop_height) {
+
+                      batch.crop(parseInt(request.query.crop_width), parseInt(request.query.crop_height));
+
+                    }
+
+                    batch.writeFile(img_path, function (err) {
+                      resolve();
+                    });
+
+                  }
+                });
+
+              });
+            }).then(function () {
+              return new Promise(function (res) {
+                fileCreated.emit(img_path, 'GAMMA');
+                reply.file(img_path);
+              });
+            });
+
+          } else {
+            reply('invalid image');
+          }
 
         });
-      }).then(function () {
-        return new Promise(function (res) {
-          fileCreated.emit(img_path, 'GAMMA');
-          reply.file(img_path);
-        });
       });
+
     },
     validate: {
       query: {
